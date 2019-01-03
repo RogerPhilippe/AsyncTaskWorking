3e678c250f408b5a04454bec9a56ee90522e98e8
rogerphilippe <roger.pereira@mc1.com.br>
2019-01-03 10:22:09 -0200

    Criada classe TarefaDownloads fora classe principal.

--- app/src/main/java/com/example/rpereira/asynctaskworking/MainActivity.java
+++ app/src/main/java/com/example/rpereira/asynctaskworking/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView mIvMain;
+    public static ImageView mIvMain;
 
     private ProgressDialog mLoad;
 
@@ -31,50 +31,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void chamarAsyncTask(String endereco){
-        TarefaDownload download = new TarefaDownload();
+        TarefaDownload download = new TarefaDownload(MainActivity.this);
         Log.i("AsyncTask", "AsyncTask senado chamado Thread: " +
                 Thread.currentThread().getName());
         download.execute(endereco);
-    }
-
-    private class TarefaDownload extends AsyncTask<String, Void, Bitmap> {
-
-        @Override
-        protected void onPreExecute(){
-            Log.i("AsyncTask", "Exibindo ProgressDialog na tela Thread: " +
-                    Thread.currentThread().getName());
-            mLoad = ProgressDialog.show(MainActivity.this, "Por favor Aguarde ...",
-                    "Baixando Imagem ...");
-        }
-
-        @Override
-        protected Bitmap doInBackground(String... params) {
-
-            Bitmap imageBitmap = null;
-
-            try {
-                Log.i("AsyncTask", "Baixando a imagem Thread: " +
-                        Thread.currentThread().getName());
-                imageBitmap = Auxiliar.baixarImagem(params[0]);
-            } catch (Exception er) { er.printStackTrace(); }
-
-            return imageBitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            if(bitmap!=null) {
-                mIvMain.setImageBitmap(bitmap);
-                Log.i("AsyncTask", "Exibindo Bitmap Thread: " + Thread.currentThread().getName());
-            } else {
-                Log.i("AsyncTask", "Erro ao baixar a imagem " +
-                        Thread.currentThread().getName());
-            }
-            Log.i("AsyncTask", "Tirando ProgressDialog da tela Thread: " +
-                    Thread.currentThread().getName());
-            mLoad.dismiss();
-        }
-
     }
 
 }
---
+++ app/src/main/java/com/example/rpereira/asynctaskworking/TarefaDownload.java
@@ -0,0 +1,55 @@
+package com.example.rpereira.asynctaskworking;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class TarefaDownload extends AsyncTask<String, Void, Bitmap> {
+
+    private ProgressDialog mLoad;
+
+    private Context mContext;
+
+    public TarefaDownload(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    protected void onPreExecute(){
+        Log.i("AsyncTask", "Exibindo ProgressDialog na tela Thread: " +
+                Thread.currentThread().getName());
+        mLoad = ProgressDialog.show(mContext, "Por favor Aguarde ...",
+                "Baixando Imagem ...");
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... params) {
+
+        Bitmap imageBitmap = null;
+
+        try {
+            Log.i("AsyncTask", "Baixando a imagem Thread: " +
+                    Thread.currentThread().getName());
+            imageBitmap = Auxiliar.baixarImagem(params[0]);
+        } catch (Exception er) { er.printStackTrace(); }
+
+        return imageBitmap;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        if(bitmap!=null) {
+            MainActivity.mIvMain.setImageBitmap(bitmap);
+            Log.i("AsyncTask", "Exibindo Bitmap Thread: " + Thread.currentThread().getName());
+        } else {
+            Log.i("AsyncTask", "Erro ao baixar a imagem " +
+                    Thread.currentThread().getName());
+        }
+        Log.i("AsyncTask", "Tirando ProgressDialog da tela Thread: " +
+                Thread.currentThread().getName());
+        mLoad.dismiss();
+    }
+
+}
